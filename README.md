85155639b9cf2a059173c1fc81ffea53603b36e1
Alberto Del Viejo <albertodelviejo@Albertos-Mac-mini.local>
2023-02-10 11:50:52 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flameflutter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flameflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flameflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
