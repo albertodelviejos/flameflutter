c54c1ca0a5c85805d701e90810081f2b9a36d249
Alberto Del Viejo <albertodelviejo@Albertos-Mac-mini.local>
2023-02-10 12:56:04 +0400

    made game

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flame/game.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flameflutter/overlays/game_over.dart';
+import 'package:flameflutter/overlays/main_menu.dart';
+import 'package:flutter/material.dart';
+
+import 'ember_quest.dart';
 
 void main() {
-  final game = FlameGame();
-  runApp(GameWidget(game: game));
+  runApp(
+    GameWidget<EmberQuestGame>.controlled(
+      gameFactory: EmberQuestGame.new,
+      overlayBuilderMap: {
+        'MainMenu': (_, game) => MainMenu(game: game),
+        'GameOver': (_, game) => GameOver(game: game),
+      },
+      initialActiveOverlays: const ['MainMenu'],
+    ),
+  );
 }
